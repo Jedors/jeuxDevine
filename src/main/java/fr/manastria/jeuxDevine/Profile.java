48de6712d5140dae12ec97b9cb78d470b070714b
jpdemory <jpdemory@manastria.fr>
2015-12-07 15:01:38 +0100

    Ajout du commentaire pour la classe Profile

--- src/main/java/fr/manastria/jeuxDevine/Profile.java
+++ src/main/java/fr/manastria/jeuxDevine/Profile.java
@@ -1,5 +1,10 @@
 package fr.manastria.jeuxDevine;
 
+/**
+ * Vous n'êtes pas obligé d'utiliser cette classe.
+ * Elle sert d'exemple pour les étudiants qui veulent utilser l'enumeration en Java
+ */
+
 public enum Profile {
 	PASMALIN("Pas malin"),
 	CHANCEUX("Chanceux"),
