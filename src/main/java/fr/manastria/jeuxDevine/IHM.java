de7bb3387b3c7ee26eed8bf4f99e29aef29ae58e
Jerome Gerard <jerome.gerard@etud.u-picardie.fr>
2016-02-23 15:49:55 +0100

    Programmation de la classe IHM

--- src/main/java/fr/manastria/jeuxDevine/IHM.java
+++ src/main/java/fr/manastria/jeuxDevine/IHM.java
@@ -4,6 +4,9 @@ import fr.manastria.utils.Console;
 
 /**
  * Created by Jean-Philippe on 31/10/2015.
+ * 
+ * Edited by Jerome on 23/02/2016
+ * @version 1.0
  */
 public class IHM {
 	public void jeux() {
@@ -24,10 +27,48 @@ public class IHM {
 		// on demande au jeu de tirer un nombre aléatoire
 		devine.determineNombreSecret();
 
+                
+                
+                int result = -2, value;
+        
+        do{
+            value = Console.readInteger("Tentez un nombre : ");
+            
+            if (value >= 0){
+                result = devine.nombreJoue(value);
+                
+                if (result > 0)
+                {
+                    System.out.println("Trop grand");
+                } else if (result < 0){
+                    System.out.println("Trop petit");
+                }
+                else
+                {
+                    System.out.println("Vous avez gagnez !");
+                }
+            }
+            else
+            {
+                result = -2;
+            }
+            
+            
+            
+        } while ((result != 0) && (result != -2));
 		// séquence d'interactions avec l'utilisateur
 		// jusqu'à ce que l'utilisateur trouve le nombre secret
 		// ou abandonne.
 
+                
+                if (value != -2)
+        {
+            System.out.println(devine.getProfile().toString());
+        }
+        else
+        {
+            System.out.println("Partie abandonnee");
+        }
 		// Affichage du profile
 	}
 }
